8b0e50bef74419fb7c6118b509c22ea199bc4d2c
Bartek <bartek4674@gmail.com>
2011-04-11 03:40:10 -0700

    zmiany

--- mapReduceViews.js
+++ mapReduceViews.js
@@ -6,21 +6,21 @@ ddoc = {
 module.exports = ddoc;
 
 ddoc.views.by_isbn = {
-	map: function(doc) {
-		if(doc.isbn13 == "")
-			emit("one", 1);
-		else
-			emit("two", 1);
-		},
-	reduce: "_sum"
+  map: function(doc) {
+    if(doc.isbn13 == "")
+      emit("one", 1);
+    else
+      emit("two", 1);
+  },
+  reduce: "_sum"
 }
 
 ddoc.views.by_numbers = {
-	map: function(doc) {
-			var cyferki = doc.isbn.split("");
-			for(var i in cyferki)
-				emit(i, 1);
-		},
-	reduce: "_sum"
+  map: function(doc) {
+    var cyferki = doc.isbn.split("");
+    for(var i in cyferki)
+      emit(i, 1);
+    },
+    reduce: "_sum"
 }
 
