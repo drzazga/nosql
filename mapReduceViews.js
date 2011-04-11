139c5a6615d94bd28719ee4aed3a6691734b7cb1
Bartosz Drzazgowski <bdrzazgo@sigma.inf.ug.edu.pl>
2011-04-11 12:35:27 +0200

    uzupelniono README

---
+++ mapReduceViews.js
@@ -0,0 +1,26 @@
+couchapp = require('couchapp');
+ddoc = {
+				    _id: '_design/app'
+										  , views: {}
+}
+module.exports = ddoc;
+
+ddoc.views.by_isbn = {
+	map: function(doc) {
+		if(doc.isbn13 == "")
+			emit("one", 1);
+		else
+			emit("two", 1);
+		},
+	reduce: "_sum"
+}
+
+ddoc.views.by_numbers = {
+	map: function(doc) {
+			var cyferki = doc.isbn.split("");
+			for(var i in cyferki)
+				emit(i, 1);
+		},
+	reduce: "_sum"
+}
+
