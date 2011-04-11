6b7349d31a68833f53cc5f2396312e87dffc402a
Bartek <bartek4674@gmail.com>
2011-04-11 04:00:28 -0700

    Edited mapReduceViews.js via GitHub

--- mapReduceViews.js
+++ mapReduceViews.js
@@ -5,7 +5,6 @@ ddoc = {
 }
 module.exports = ddoc;
 
-<<<<<<< HEAD
 ddoc.views.by_date = {
 	map: function(doc) {
 		emit(doc.released.split("-"), 1);
@@ -18,24 +17,5 @@ ddoc.views.by_runtime = {
 	  emit([doc.runtime, doc.released.split("-")[0]], 1);
 	     },
 	reduce: "_sum"
-=======
-ddoc.views.by_isbn = {
-  map: function(doc) {
-    if(doc.isbn13 == "")
-      emit("one", 1);
-    else
-      emit("two", 1);
-  },
-  reduce: "_sum"
-}
 
-ddoc.views.by_numbers = {
-  map: function(doc) {
-    var cyferki = doc.isbn.split("");
-    for(var i in cyferki)
-      emit(i, 1);
-    },
-    reduce: "_sum"
->>>>>>> 09a3050ea0e7f76345493e414498a45c2879c095
-}
 
